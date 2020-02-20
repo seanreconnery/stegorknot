6d266bb44dd6262b2616c59049966ec2db1216e1
seanreconnery <56751730+seanreconnery@users.noreply.github.com>
2020-02-19 18:11:43 -0800

    reduced max file size

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + "/img2scan"
 app = Flask(__name__)
 app.secret_key = "app1key4220k3Vk3y4pphft3wo85dtkhyu"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
