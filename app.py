554f3254c3102eab6faf0575e7e927704905dd07
seanreconnery <56751730+seanreconnery@users.noreply.github.com>
2020-02-20 00:21:30 -0800

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask
-
-UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + "/img2scan"
-
-app = Flask(__name__)
-app.secret_key = "app1key4220k3Vk3y4pphft3wo85dtkhyu"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
