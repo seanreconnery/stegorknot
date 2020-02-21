9b1250208fd6d855cd94007bb744181f952eca61
seanreconnery <56751730+seanreconnery@users.noreply.github.com>
2020-02-21 10:49:52 -0800

    added check for embedded/appened files

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import os
 from app import app
 from flask import request, jsonify
 from werkzeug.utils import secure_filename
+from struct import unpack
+from wand.image import Image
 
 # limit to JPG images because thats all PIXELKNOT handles iirc
-ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['gif', 'png', 'pdf', 'jpg', 'jpeg'])
+
+filename = ""
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -44,12 +48,14 @@ def upload_file():
 			s = f.read()
 		# PIXEL KNOT byte string
 		found = s.find(b'\xFF\xC0\x00\x11\x08')
+		wbStego = s.find(b'\x00\xFF')
+
 		if found == -1:
 			# not found -- this byte string always happens around the same series/place in the file
-			resp = jsonify({'PixelKnot' : 'NO'})
+			resp = jsonify({'PixelKnot':'NO'})
 		else:
 			# Byte stirng was found, PixelKnot chances are high
-			resp = jsonify({'PixelKnot': 'YES'})
+			resp = jsonify({'PixelKnot':'YES'})
 
 		resp.status_code = 201
 		return resp
@@ -57,6 +63,215 @@ def upload_file():
 		resp = jsonify({'error' : 'Allowed file types are jpg or jpeg'})
 		resp.status_code = 400
 		return resp
+
+
+# route to API, accepts POST request with multi part form data
+@app.route('/api/embed', methods=['POST'])
+def embed():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp2 = jsonify({'error': 'No file part in the request'})
+		resp2.status_code = 400
+		return resp2
+	file = request.files['file']
+	if file.filename == '':
+		resp2 = jsonify({'error': 'No file selected for uploading'})
+		resp2.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		imgfile = app.config['UPLOAD_FOLDER'] + "/" + filename
+		# open the image, read in a
+		f = open(imgfile, 'rb')
+		# read in the image
+		s = f.read()
+		# wbStego byte string
+		#wbStego = s.find(b'\x00\xFF')
+
+		png = s.find(b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", 2)
+		jpg = s.find(b"\xff\xd8\xff\xe0", 2)
+		gif = s.find(b"\x47\x49\x46\x38\x39\x61", 2)
+		zip = s.find(b"\x50\x3b\x03\x04")
+		rar = s.find(b"\x52\x61\x72\x21\x1a\x07")	# rar
+		z7z = s.find(b"\x37\x7a\xbc\xaf\x27\x1c")	# 7zip
+		sqlite = s.find(b"\x53\x51\x4c\x69\x74\x65\x20\x66\x6f\x72\x6d\x61\x74\x20\x33\x00")	# SQLite database
+		telgd = s.find(b"\x54\x44\x46\x24")	# telegram desktop file
+		telenc = s.find(b"\x54\x44\x45\x46")	# telegram encrypted file
+		pdf = s.find(b"\x25\x50\x44\x46\x2d")
+		lzip = s.find(b"\x4C\x5A\x49\x50")
+		pkzip = s.find(b"\x50\x4B\x03\x04")
+		utf8 = s.find(b"\xEF\xBB\xBF")
+		tar = s.find(b"\x75\x73\x74\x61\x72")
+		xzlz = s.find(b"\xFD\x37\x7A\x58\x5A\x00\x00")
+		doc = s.find(b"\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1")
+
+		resp2 = ''
+		resp_jpg = ''
+		resp_png = ''
+		resp_gif = ''
+		resp_zip = ''
+		resp_rar = ''
+		resp_z7z = ''
+		resp_sql = ''
+		resp_tel = ''
+		resp_ten = ''
+		resp_lz = ''
+		resp_pkz = ''
+		resp_utf = ''
+		resp_xz = ''
+		resp_tar = ''
+		resp_doc = ''
+
+		if png == -1 & jpg == -1 & gif == -1 & zip == -1 & rar == -1 & z7z == -1 & sqlite == -1 & telgd == -1 & telenc == -1 & lzip == -1 & pkzip == -1 & pdf == -1 & utf8 == -1 & tar == -1 & xzlz == -1 & doc == -1:
+			# nothing found
+			resp2 = jsonify({'Embedded':'NO'})
+			resp2.status_code = 201
+			return resp2
+
+
+		if zip != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(zip)
+			dat = fdpng.read()
+			#x = dat.find(b"\x06\x05\x4b\x50")
+			resp_zip = "{'ZIP':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if pkzip != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(pkzip)
+			dat = fdpng.read()
+			#x = dat.find(b"\x06\x05\x4b\x50")
+			#xend = dat.endswith(b"\x06\x05\x4b\x50")
+			resp_pkz = "{'PKZip':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if utf8 != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(utf8)
+			dat = fdpng.read()
+			resp_utf = "{'UTF-8 Text':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if xzlz != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(xzlz)
+			dat = fdpng.read()
+			resp_xz = "{'XZ/LZMA Compressed Archive':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if rar != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(rar)
+			dat = fdpng.read()
+			print("RAR :" + str(dat.__sizeof__()))
+			num = dat.count(b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", 1, dat.__sizeof__())
+			resp_rar = "{'RAR':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if png != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			pngb = fdpng.read()
+			pngoffset = pngb.find(b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", 2)  # Magic bytes
+			endofpng = pngb.find(b"\x49\x45\x4E\x44\xAE\x42\x60\x82", pngoffset)
+			fdpng.seek(pngoffset)
+			png_length = unpack('i', fdpng.read(4))  # Read size of JPEG data
+			dat = fdpng.read(png_length[0])
+			x = dat.endswith(b"\x49\x45\x4E\x44\xAE\x42\x60\x82")
+
+			if x == True:
+				resp_png = "{'PNG':'" + str(dat.__sizeof__()) + " bytes'}"
+			else:
+				fdpng.seek(endofpng)
+				ep = fdpng.read()
+				print(ep.__sizeof__())
+				xlen = int(dat.__sizeof__()) - int(ep.__sizeof__())
+				print("PNG :" + str(xlen))
+
+				resp_png = "{'PNG':'approx " + str(xlen) + " bytes'}"
+
+		if z7z != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(z7z)
+			dat = fdpng.read()
+			resp_z7z = "{'7Zip Compressed Archive':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if pdf != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(pdf)
+			dat = fdpng.read()
+			resp_pdf = "{'PDF':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if sqlite != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(sqlite)
+			dat = fdpng.read()
+			resp_sql = "{'SQLite Database':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if telgd != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(telgd)
+			dat = fdpng.read()
+			resp_tel = "{'Telegram Desktop File':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if telenc != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(telenc)
+			dat = fdpng.read()
+			resp_ten = "{'Telegram Encrypted File':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if lzip != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(lzip)
+			dat = fdpng.read()
+			resp_lz = "{'LZIP Compressed Archive':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if jpg != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(jpg)
+			dat = fdpng.read()
+			resp_jpg = "{'JPEG/JPG Image':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if gif != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(gif)
+			dat = fdpng.read()
+			resp_gif = "{'GIF Image':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if tar != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(tar)
+			dat = fdpng.read()
+			resp_tar = "{'TAR Compressed Archive':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+		if doc != -1:
+			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
+			fdpng.seek(doc)
+			dat = fdpng.read()
+			resp_doc = "{'MS Office File':'" + str(dat.__sizeof__()) + " bytes'}"
+
+
+
+		resp2 = jsonify({'Embedded Files':'[' + resp_jpg + resp_png + resp_gif + resp_zip + resp_tar + resp_rar + resp_z7z + resp_sql + resp_pkz + resp_tel + resp_ten + resp_lz + resp_doc + resp_xz + resp_utf + ']'})
+		resp2.status_code = 201
+
+		return resp2
+
+	else:
+		resp2 = jsonify({'error': 'Allowed file types are jpg or jpeg'})
+		resp2.status_code = 400
+		return resp2
+
 
 if __name__ == "__main__":
     app.run()
