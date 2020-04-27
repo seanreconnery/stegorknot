4d3dd21c82c2d0d9f12cc0e5888f656af7157272
seanreconnery <56751730+seanreconnery@users.noreply.github.com>
2020-04-26 17:48:16 -0700

    added some commenting

--- main.py
+++ main.py
@@ -4,56 +4,63 @@ from werkzeug.utils import secure_filename
 from struct import unpack
 from wand.image import Image
 
-UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + "/img2scan"
+UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + "/img2scan"	# set a folder to upload images to
+# I have a cronjob on my server to delete all the uploaded images, but it's probably smarter to delete after the scan is done..
 
 app = Flask(__name__)
-app.secret_key = "app1key4220k3Vk3y4pphft3wo85dtkhyu"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
-# limit to JPG images because thats all PIXELKNOT handles iirc
-ALLOWED_EXTENSIONS = set(['gif', 'png', 'pdf', 'jpg', 'jpeg'])
+app.secret_key = "YOUR_SECRET_KEY_HERE"			# you can just make something up.
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER		# set the upload folder.
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024	# set a limit of 8mb file size.
 
-filename = ""
+ALLOWED_EXTENSIONS = set(['gif', 'png', 'pdf', 'jpg', 'jpeg'])	# limit filetypes to image formats
+
+
+filename = ""		# define an empty variable for the file that's being scanned
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
-	# build HTML response for anyone not using the API
+	# build HTML response for anyone not using the API properly (and trying to view as a webpage)
 	html = "<html><body valign='middle'><center>"
-	html = html + "<div width='40%'></div>"
-	html = html + "<div style='align:center;width:20%;padding:25px;margin:50px;border-radius:20px;background:#e8e8e8;'>"
-	html = html + "<center><h1>StegOrKnot</h1>"
-	html = html + "<h4>This is not the endpoint you're looking for...</h4><br>"
-	html = html + "<br><b><em>Resource Endpoint</em>:</b>  /api/scan<br><br></center>"
-	html = html + "</div><div width='40%'></div></center></body</html>"
-	return html
+	html += "<div width='40%'></div>"
+	html += "<div style='align:center;width:20%;padding:25px;margin:50px;border-radius:20px;background:#e8e8e8;'>"
+	html += "<center><h1>StegOrKnot</h1>"
+	html += "<h4>This is not the endpoint you're looking for...</h4><br>"
+	html += "<ul><b><em>Resource Endpoints</em>:</b>"
+	html += "<li><b>/api/scan</b><br><em>*check for PixelKnot</em></li>"
+	html += "<li><b>/api/embed</b><br><em>*check for appended files</em></li>"
+	html += "</ul></center>"
+	html += "</div><div width='40%'></div></center></body</html>"
+	return html		# return the string of HTML for the browser to display
 
-# route to API, accepts POST request with multi part form data
+# route to PixelKnot scan, accepts POST request with multi part form data
 @app.route('/api/scan', methods=['POST'])
-def upload_file():
+def check_PixelKnot():
 	# check if the post request has the file part
-	if 'file' not in request.files:
+	if 'file' not in request.files:		# file didn't attach for some reason
 		resp = jsonify({'error' : 'No file part in the request'})
 		resp.status_code = 400
 		return resp
 	file = request.files['file']
-	if file.filename == '':
+	if file.filename == '':			# no file selected
 		resp = jsonify({'error' : 'No file selected for uploading'})
 		resp.status_code = 400
 		return resp
-	if file and allowed_file(file.filename):
+	if file and allowed_file(file.filename):	# make sure the filetype is allowed
 		filename = secure_filename(file.filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 		imgfile = app.config['UPLOAD_FOLDER'] + "/" + filename
-		# open the image, read in a
+		# open the image to read as bytes
 		with open(imgfile, 'rb') as f:
 			# read in the image
 			s = f.read()
 		# PIXEL KNOT byte string
-		found = s.find(b'\xFF\xC0\x00\x11\x08')
-		wbStego = s.find(b'\x00\xFF')
+		found = s.find(b'\xFF\xC0\x00\x11\x08')	# byte string present in all pixelknot processed images
+							# there's a possibility of false positives of course
+			
+		wbStego = s.find(b'\x00\xFF')		# will work on this later
 
 		if found == -1:
 			# not found -- this byte string always happens around the same series/place in the file
@@ -70,9 +77,9 @@ def upload_file():
 		return resp
 
 
-# route to API, accepts POST request with multi part form data
+# scan for appended files, accepts POST request with multi part form data
 @app.route('/api/embed', methods=['POST'])
-def embed():
+def check_Embed():
 	# check if the post request has the file part
 	if 'file' not in request.files:
 		resp2 = jsonify({'error': 'No file part in the request'})
@@ -94,23 +101,25 @@ def embed():
 		# wbStego byte string
 		#wbStego = s.find(b'\x00\xFF')
 
+		# byte strings / Magic File Numbers for a variety of file types
 		png = s.find(b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", 2)
 		jpg = s.find(b"\xff\xd8\xff\xe0", 2)
 		gif = s.find(b"\x47\x49\x46\x38\x39\x61", 2)
-		zip = s.find(b"\x50\x3b\x03\x04")
+		zipf = s.find(b"\x50\x3b\x03\x04")
 		rar = s.find(b"\x52\x61\x72\x21\x1a\x07")	# rar
 		z7z = s.find(b"\x37\x7a\xbc\xaf\x27\x1c")	# 7zip
 		sqlite = s.find(b"\x53\x51\x4c\x69\x74\x65\x20\x66\x6f\x72\x6d\x61\x74\x20\x33\x00")	# SQLite database
-		telgd = s.find(b"\x54\x44\x46\x24")	# telegram desktop file
-		telenc = s.find(b"\x54\x44\x45\x46")	# telegram encrypted file
-		pdf = s.find(b"\x25\x50\x44\x46\x2d")
-		lzip = s.find(b"\x4C\x5A\x49\x50")
-		pkzip = s.find(b"\x50\x4B\x03\x04")
-		utf8 = s.find(b"\xEF\xBB\xBF")
-		tar = s.find(b"\x75\x73\x74\x61\x72")
-		xzlz = s.find(b"\xFD\x37\x7A\x58\x5A\x00\x00")
-		doc = s.find(b"\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1")
+		telgd = s.find(b"\x54\x44\x46\x24")		# telegram desktop file
+		telenc = s.find(b"\x54\x44\x45\x46")		# telegram encrypted file
+		pdf = s.find(b"\x25\x50\x44\x46\x2d")		# pdf file
+		lzip = s.find(b"\x4C\x5A\x49\x50")		# LZip archive
+		pkzip = s.find(b"\x50\x4B\x03\x04")		# PKZip compressed archive
+		utf8 = s.find(b"\xEF\xBB\xBF")			# UTF-8 encoded data/text
+		tar = s.find(b"\x75\x73\x74\x61\x72")		# TAR archive
+		xzlz = s.find(b"\xFD\x37\x7A\x58\x5A\x00\x00")		# XZ compressed archive
+		doc = s.find(b"\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1")	# Microsoft Office file
 
+		# define some empty variables to hold a yes/no RE: embedded file
 		resp2 = ''
 		resp_jpg = ''
 		resp_png = ''
@@ -135,7 +144,7 @@ def embed():
 			return resp2
 
 
-		if zip != -1:
+		if zipf != -1:
 			fdpng = open(app.config['UPLOAD_FOLDER'] + "/" + filename, "rb")
 			fdpng.seek(zip)
 			dat = fdpng.read()
@@ -273,7 +282,8 @@ def embed():
 		return resp2
 
 	else:
-		resp2 = jsonify({'error': 'Allowed file types are jpg or jpeg'})
+		# 
+		resp2 = jsonify({'error': 'Incompatible file type--only image formats allowed.'})
 		resp2.status_code = 400
 		return resp2
 
